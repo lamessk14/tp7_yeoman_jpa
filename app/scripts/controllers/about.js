ffecfa33736bb2ba9a935add5d433c1acf713cb7
Unknown <louise-agnes.essebe@etudiant.univ-rennes1.fr>
2018-03-26 18:48:05 +0200

    Creation du projet
    Creation de l'ossature du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sirApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sirApp
+ */
+angular.module('sirApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sirApp
+ */
+angular.module('sirApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
